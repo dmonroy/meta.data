e1e379ab70b23326b73b4a0dd09b515509e11d2b
Darwin Monroy <contact@darwinmonroy.com>
2016-01-08 19:01:15 -0600

    module for database tests

---
+++ tests/test_db.py
@@ -0,0 +1,35 @@
+import json
+
+import psycopg2
+from pytest import fixture
+
+from meta.data.pg import parse_pgurl
+from meta.data.settings import get_settings
+
+
+@fixture(scope='module')
+def settings():
+    return get_settings()
+
+
+@fixture(scope='module')
+def conn(request, settings):
+
+    db_data = parse_pgurl(settings.db_url)
+    _conn = psycopg2.connect(**db_data)
+
+    def fin():
+        _conn.close()
+    request.addfinalizer(fin)
+    return _conn
+
+
+def test_insert(conn):
+    data = dict(
+        source='test_insert',
+        tags=['test', 'insert']
+    )
+    c = conn.cursor()
+    c.execute("insert into meta (data) values (%s)", (json.dumps(data),))
+    c.execute("select count(1) from meta where data ->> 'source' = 'test_insert'")
+    assert c.fetchone()[0] == 1
