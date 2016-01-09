d3e47de9134fd48dc2e33e10bd4aef5f81e99a67
Darwin Monroy <contact@darwinmonroy.com>
2016-01-08 21:21:14 -0600

    Text the index of the main endpoint

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,3 +1,23 @@
+from aiohttp import request
+
+import meta.data
+
+from chilero.web.test import WebTestCase, asynctest
+
+
 def test_imports():
     # It doesn't fails when importing meta.data
     import meta.data
+
+
+class TestMeta(WebTestCase):
+    routes = meta.data.routes
+
+    @asynctest
+    def test_index(self):
+        resp = yield from request(
+            'GET', self.full_url('/'), loop=self.loop,
+        )
+
+        self.assertEqual(resp.status,200)
+        resp.close()
